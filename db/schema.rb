1f7413c8fde17e40eb136c56a47214b272be7330
Mat Alan Gilbert <matalangilbert@gmail.com>
2012-06-14 12:50:23 +0100

    Submit

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120614112532) do
+ActiveRecord::Schema.define(:version => 20120614114538) do
 
   create_table "messages", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "content"
   end
 
 end
