5ee77e0d050850cea8b51a33857db2dbd0214ff8
Mat Alan Gilbert <matalangilbert@gmail.com>
2012-06-14 12:28:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Talker::Application.config.secret_token = '26434d7efccdcad928b5e6716d120b69ad674f19fb765ebf4da4ffff33d6d12b52698d0f9ef9b697a89c09c8cbd27af82c559285afbc0086840e35d5d03c9638'
