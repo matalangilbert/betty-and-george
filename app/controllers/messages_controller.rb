3713faed1d4b6d3daba3d03b9d234f539276ff41
Mat Alan Gilbert <matalangilbert@gmail.com>
2012-06-14 15:34:21 +0100

    javascript now included

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,17 @@
 class MessagesController < ApplicationController
+  respond_to :html, :js
+  
   def index
     @messages = Message.all
   end
 
   def create
-    @message = Message.create!(params[:message])
+    @message = Message.new(params[:message])
+    
+    respond_to do |format|
+      format.html {redirect_to :action => 'index', :notice => 'Not AJAX'}
+      format.js
+    end
+    
   end
 end
